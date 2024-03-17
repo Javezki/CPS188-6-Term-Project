ae1f8d99315a8c278d6810228354d020ed4d3dc2
javezki <liunathan87@gmail.com>
2024-03-16 22:38:37 -0400

    remove dyn alloc. Read all data from csv and seperates into individual arrays

--- main.c
+++ main.c
@@ -3,70 +3,128 @@
 #include <string.h>
 
 #define DATE_SIZE 10
+#define DATA_COUNT 3193
 
 
 // Accont's for header and skips over it
 int counter = -1;
 
-// Data Array
-char (*dt)[DATE_SIZE];
-// Land Average Temperature
-double *LAT;
+// Date 
+char dt[DATA_COUNT][DATE_SIZE];
+// Land Average Temperature 
+double lat[DATA_COUNT];
 // Land Average Temperature Uncertainty
-double *LATU;
+double latu[DATA_COUNT];
 // Land Max Temperature
-double *LMT;
+double lmt[DATA_COUNT];
 // Land Max Temperature Uncertainty
-double *LMTU;
+double lmtu[DATA_COUNT];
 // Land Min Temperature
-double *LMIT;
+double lmit[DATA_COUNT];
 // Land Min Temperature Uncertainty
-double *LMITU;
+double lmitu[DATA_COUNT];
 // Land and Ocean Average Temperature
-double *LOAT;
+double loat[DATA_COUNT];
 // Land and Ocean Average Temperature Uncertainty
-double *LOATU;
-
+double loatu[DATA_COUNT];
+// Ocean Average Temperature
+double oat[DATA_COUNT];
+// Ocean Average Temperature Uncertainty
+double oatu[DATA_COUNT];
 
 /**
  * Function to get the count of data entries in a file.
  * This function reads a file named "GlobalTemperatures.csv" and counts the total entries of data.
  * It also prints out each line of data while counting.
- * @note Might remove this if global temp file is guarenteed a certain size 
+ * @note Might remove this if global temp file is guaranteed a certain size 
  * @return void
  */
-void initializeVariables() {
-    // If we want dynamic allocation of memory this can get complicated so just stick with 1024 till it breaks
-    char line[1024];
-    
-    FILE *file = fopen("GlobalTemperatures.csv", "r");
-
-    // Checks if file exists
+void setValuesFromFile() {
+    FILE *file;
+    file = fopen("GlobalTemperatures.csv", "r");
     if (file == NULL) {
         printf("Error: File not found\n");
-        return(1);
+        return 1;
     }
+    // Line shouldn't be more than 1024 characters? (Idk maybe lol)
+    char line[1024];
+    while (fgets(line, 1024, file)) {
+        // Skip header
+        if (counter == -1) {
+            counter++;
+            continue;
+        }
+        // Split the line into tokens using comma as the delimiter
+        char *token = strtok(line, ",");
+        int index = 0;
 
-    // Counts total entries of data and prints out data (May remove print part later)
-    while (fgets(line, sizeof(line), file)) {
+        // Iterate through each token and assign values to the corresponding arrays
+        while (token != NULL) {
+            switch (index) {
+                case 0:
+                    // Copy the date string to the date array
+                    strncpy(dt[counter], token, DATE_SIZE);
+                    break;
+                case 1:
+                    // Convert the string to a double and assign it to lat array
+                    lat[counter] = atof(token);
+                    break;
+                case 2:
+                    // Convert the string to a double and assign it to latu array
+                    latu[counter] = atof(token);
+                    break;
+                case 3:
+                    // Convert the string to a double and assign it to lmt array
+                    lmt[counter] = atof(token);
+                    break;
+                case 4:
+                    // Convert the string to a double and assign it to lmtu array
+                    lmtu[counter] = atof(token);
+                    break;
+                case 5:
+                    // Convert the string to a double and assign it to lmit array
+                    lmit[counter] = atof(token);
+                    break;
+                case 6:
+                    // Convert the string to a double and assign it to lmitu array
+                    lmitu[counter] = atof(token);
+                    break;
+                case 7:
+                    // Convert the string to a double and assign it to loat array
+                    loat[counter] = atof(token);
+                    break;
+                case 8:
+                    // Convert the string to a double and assign it to loatu array
+                    loatu[counter] = atof(token);
+                    break;
+                case 9:
+                    // Convert the string to a double and assign it to oat array
+                    oat[counter] = atof(token);
+                    break;
+                case 10:
+                    // Convert the string to a double and assign it to oatu array
+                    oatu[counter] = atof(token);
+                    break;
+                default:
+                    break;
+            }
+            // Doesn't immediately set value to null, instead checks first string and moves onto next token
+            // until there are no other tokens to view 
+            token = strtok(NULL, ",");
+            index++;
+        }
+
+        // Increment the counter after processing each line
         counter++;
-        printf("%s", line);
     }
-    // Manually allocates memory for each array based on the count of data entries and data type
-    dt = malloc(counter * sizeof(char[DATE_SIZE]));
-    LAT = malloc(sizeof(double) * counter);
-    LATU = malloc(sizeof(double) * counter);
-    LMT = malloc(sizeof(double) * counter);
-    LMTU = malloc(sizeof(double) * counter);
-    LMIT = malloc(sizeof(double) * counter);
-    LMITU = malloc(sizeof(double) * counter);
-    LOAT = malloc(sizeof(double) * counter);
-    LOATU = malloc(sizeof(double) * counter);
-
+    fclose(file);
+    printf("Data count: %d\n", counter);
 }
+
+
 
 int main(void) {
     // Call function to get data count
-    initializeVariables();
+    setValuesFromFile();
     return 0;
 }
